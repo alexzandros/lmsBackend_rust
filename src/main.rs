33502107bfadb966e1a7f37c21b71430e8236d97
Carlos Roberto Rojas <carlosroberto.rojas@gmail.com>
2018-06-19 21:04:40 -0500

    Post de ususario realizado

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate uuid;
 extern crate postgres;
+#[macro_use]
 extern crate serde_json;
 extern crate zmq;
 
@@ -28,16 +29,30 @@ fn enrutar(peticion:String) -> String{
     match verbo{
         "listar_usuarios" =>
             listar_usuarios(cadenas[1].parse().unwrap(),
-                cadenas[2].parse().unwrap()),
+                cadenas[2].parse().unwrap()).to_string(),
+        "insertar_usuario" =>{ 
+            insertar_usuario(&peticion[16..]).to_string()},
         _ => String::from("Método no definido")
     }
 }
 
-fn listar_usuarios(num_pag: i32, elem_por_pagina:i32) -> String{
+fn listar_usuarios(num_pag: i32, elem_por_pagina:i32) -> Value{
     let cadena = String::from("postgres://logica_ludica:seguridad_777@localhost/lms_l_l");
     let conexion = Connection::connect(cadena, TlsMode::None).unwrap();
     let respuesta_json:Value = 
         (&conexion.query("SELECT listar_usuarios($1, $2)",
         &[&num_pag,&elem_por_pagina]).unwrap()).get(0).get(0);
-    respuesta_json.to_string()
+    respuesta_json
+}
+
+fn insertar_usuario(cadena_usuario: &str) -> Value{
+    let usuario:Value = serde_json::from_str(cadena_usuario).unwrap();
+    let cadena_conexion =
+        String::from("postgres://logica_ludica:seguridad_777@localhost/lms_l_l");
+    let conexion = Connection::connect(cadena_conexion, TlsMode::None).unwrap();
+    let respuesta:uuid::Uuid = 
+        conexion.query("SELECT insert_usuario($1)", &[&usuario])
+        .unwrap().get(0).get(0);
+    json!(respuesta)
+
 }
